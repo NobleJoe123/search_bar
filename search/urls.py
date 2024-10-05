7faf64ec4239684a657ee899a36fceb6c1b1ec31
NobleJoe123 <Joshuachibuike14@gmail.com>
2024-10-05 18:57:36 +0100

    search_urls_views

--- search/urls.py
+++ search/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import search_view
+from .views import search_view, search_update
 
 
 
 
 urlpatterns = [
     path('search/', search_view, name='search'),
+    path('update/<int:product_id>/', search_update, name='product_update'),
 
     
 ]
--- search/views.py
+++ search/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Search
-from .forms import SearchForm, UpdateProductForm
+from .forms import SearchForm, UpdateForm
 
 
 
@@ -16,3 +16,14 @@ def search_view(request):
         results = Search.objects.filter(name__icontains=query)
 
     return render(request, 'search/index.html', {'form': form, 'query': query, 'results': results})
+
+def search_update(request, product_id):
+    product = get_object_or_404(Search, id=product_id)
+
+    if request.method == 'POST':
+        form = UpdateForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('search')  # Redirect back to the search page
+
+    return redirect('search')
