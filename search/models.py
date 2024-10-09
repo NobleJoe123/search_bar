c5d5b9ca680191c24f9ff085bf7e19996368a02f
NobleJoe123 <Joshuachibuike14@gmail.com>
2024-10-09 18:24:21 +0100

    change model parameters

--- search/models.py
+++ search/models.py
@@ -4,8 +4,10 @@ from django.db import models
 
 class Search(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    email = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
+    phonenum = models.CharField(max_length=100)
+    age = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
